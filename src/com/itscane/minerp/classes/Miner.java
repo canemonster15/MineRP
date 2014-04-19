20884bdce7d899c1b1b07c4fa0aa7d48e45442f2
canemonster15 <brianlaymon@gmail.com>
2014-04-18 23:54:17 -0400

    update

--- src/com/itscane/minerp/classes/Miner.java
+++ src/com/itscane/minerp/classes/Miner.java
@@ -2,6 +2,7 @@ package com.itscane.minerp.classes;
 
 import java.util.HashSet;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 import com.itscane.minerp.Main;
@@ -15,7 +16,14 @@ public class Miner {
 		this.main = main;
 	}
 	
-	public int strength() {
+	public int getStrength() {
 		return 9;
 	}
+	
+	public void add(Player p) {
+		String n = p.getName();
+		miners.add(n);
+		p.sendMessage(ChatColor.BLUE + "You have become a miner!");
+	}
+	
 }
